9b68969212043e8a6f2afcf1edb54dcd5d6e5ded
JoaoPedroSilvaSantana <166645046+JoaoPedroSilvaSantana@users.noreply.github.com>
2024-04-10 16:35:25 -0300

    Create Produto.java

---
+++ Produto.java
@@ -0,0 +1,37 @@
+public class Produto {
+	private String nome;
+	private double preco;
+	private int quantidade;
+	
+	public Produto(String n, double p, int q) {
+		this.nome = n;
+		this.preco = p;
+		this.quantidade = q;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public double getPreco() {
+		return preco;
+	}
+	
+	public int getQuantidade(){
+		return quantidade;
+	}
+	
+	public void setNome(String n) {
+		this.nome = n;
+	}
+	
+	public void setPreco(double p) {
+		this.preco = p;
+	}
+	
+	public void setQuantidade(int q) {
+		this.quantidade = q;
+	}
+	
+	
+}
