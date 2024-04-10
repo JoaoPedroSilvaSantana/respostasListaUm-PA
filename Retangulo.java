e9ba0d1164d2d34b8eb8afce22c314f30b9544f2
JoaoPedroSilvaSantana <166645046+JoaoPedroSilvaSantana@users.noreply.github.com>
2024-04-10 16:39:37 -0300

    Create Retangulo.java

---
+++ Retangulo.java
@@ -0,0 +1,31 @@
+public class Retangulo {
+	
+	private double base;
+	private double altura;
+	
+	public Retangulo(double b, double a) {
+		this.base = b;
+		this.altura = a;
+	}
+	
+	public double getbase() {
+		return base;
+	}
+	
+	public double getaltura() {
+		return altura;
+	}
+	
+	public void setbase(double b) {
+		this.base = b;
+	}
+	
+	public void setaltura(double a) {
+		this.altura = a;
+	}
+	
+	public double calculararea; {
+		return base / altura;
+	}
+	
+}
